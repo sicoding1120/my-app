e9776c301570f3ffaa34c690b49b11e1392b37a5
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-09-09 13:41:50 +0700

    handle error

--- src/frontends/fragments/home/sponsor.tsx
+++ src/frontends/fragments/home/sponsor.tsx
@@ -2,7 +2,7 @@
 import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
 import Image from "next/image";
-import dataRandom from "../data/json/dataRandom.json";
+import dataRandom from "@/data/dataRandom.json";
 
 const Sponsor = () => {
   return (
