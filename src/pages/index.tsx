b6a6fb3cd6a283da4f394e1f683300aba1b23e49
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-09-09 11:31:12 +0700

    update config url post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,44 @@
-import axios from 'axios';
-import React, { useState } from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 const Index = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
-  
 
   const handleUser = async (e: any) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:3000/api/users", {
-        name,
-        password
-      });
-      window.location.reload();
+      const response = await axios.post(
+        "https://my-app-one-pi-53.vercel.app/api/users",
+        {
+          name,
+          password,
+        }
+      );
+      if (typeof window !== "undefined") {
+        window.location.reload();
+      }
       console.log(response.data);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   return (
     <div>
-      <input type="text" placeholder='username'onChange={(e) => setName(e.target.value)} />
-      <input type="text" placeholder='username' onChange={(e) => setPassword(e.target.value)} />\
-      <button onClick={handleUser}>create</button>
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      \<button onClick={handleUser}>create</button>
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
