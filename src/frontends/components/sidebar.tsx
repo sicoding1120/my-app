acf3676b97356068facf6630a161fed8efa57166
ahfary <afabqory@gmail.com>
2024-09-11 09:42:13 +0700

    edit dashboard ganteng

--- src/frontends/components/sidebar.tsx
+++ src/frontends/components/sidebar.tsx
@@ -1,8 +1,12 @@
 "use client";
 import React, { useState } from "react";
-import { Sidebar, SidebarBody, SidebarLink } from "@/frontends/components/ui/sidebar";
 import {
-    Icon24Hours,
+  Sidebar,
+  SidebarBody,
+  SidebarLink,
+} from "@/frontends/components/ui/sidebar";
+import {
+  Icon24Hours,
   IconArrowLeft,
   IconBrandTabler,
   IconPaperclip,
@@ -13,6 +17,7 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { cn } from "@/frontends/lib/util";
+import { DarkMode } from "@chakra-ui/react";
 
 export function SidebarDemo() {
   const links = [
@@ -20,35 +25,35 @@ export function SidebarDemo() {
       label: "Dashboard",
       href: "#",
       icon: (
-        <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-6 w-6 flex-shrink-0" />
       ),
     },
     {
       label: "Kelas",
       href: "#",
       icon: (
-        <IconPaperclip className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconPaperclip className="text-neutral-700 dark:text-neutral-200 h-6 w-6 flex-shrink-0" />
       ),
     },
     {
       label: "Profil",
       href: "#",
       icon: (
-        <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-6 w-6 flex-shrink-0" />
       ),
     },
     {
       label: "Pengaturan",
       href: "#",
       icon: (
-        <IconSettings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconSettings className="text-neutral-700 dark:text-neutral-200 h-6 w-6 flex-shrink-0" />
       ),
     },
     {
       label: "Keluar",
       href: "#",
       icon: (
-        <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-6 w-6 flex-shrink-0" />
       ),
     },
   ];
@@ -66,7 +71,7 @@ export function SidebarDemo() {
             {open ? <Logo /> : <LogoIcon />}
             <div className="mt-8 flex flex-col gap-2">
               {links.map((link, idx) => (
-                <SidebarLink key={idx} link={link} />
+                <SidebarLink key={idx} link={link} className="font-medium" />
               ))}
             </div>
           </div>
@@ -99,11 +104,17 @@ export const Logo = () => {
       href="#"
       className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
     >
-      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <Image
+      // isikan argument darkmode
+        src={DarkMode(  ) ? "/assets/logo/MENTORIX2.png" : "/assets/logo/MENTORIX.png"}
+        alt="logo"
+        width={50}
+        height={50}
+      />
       <motion.span
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        className="font-medium text-black dark:text-white whitespace-pre"
+        className="text-black dark:text-white whitespace-pre text-xl font-bold"
       >
         MENTORIX
       </motion.span>
@@ -116,7 +127,12 @@ export const LogoIcon = () => {
       href="#"
       className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
     >
-      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <Image
+        src={DarkMode() ? "/assets/logo/MENTORIX2.png" : "/assets/logo/MENTORIX.png"}
+        alt="logo"
+        width={50}
+        height={50}
+      />
     </Link>
   );
 };
