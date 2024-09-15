32dd324053f3feff50b4a469eae0b04de11cd526
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-09-15 14:38:49 +0700

    update 3

--- src/pages/courses/class/[[...classes]].tsx
+++ src/pages/courses/class/[[...classes]].tsx
@@ -1,31 +1,31 @@
-import DetailClasses from "@/frontends/components/detailClasses";
-import useSWR from "swr";
-import { useRouter } from "next/router";
-import React from "react";
-import materialClass from '@/data/materiOfClass.json';
+// import DetailClasses from "@/frontends/components/detailClasses";
+// import useSWR from "swr";
+// import { useRouter } from "next/router";
+// import React from "react";
+// import materialClass from '@/data/materiOfClass.json';
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 
-const Dynamic = () => {
-  const { query } = useRouter();
-  const { data, error, isLoading } = useSWR("/api/class", fetcher);
-  console.log(error, isLoading);
-  const filter = data.data.classes.filter((items: any) => items.title == query.classes?.at(0));
-  return (
-    <div>
-      <DetailClasses
-        title={query.classes?.at(0)}
-        mentorixPoin={filter[0].mentorixPoin}
-        level={filter[0].difficultyLevel}
-        time={filter[0].time}
-        lesson={filter[0].lesson}
-        desc={filter[0].aboutClass}
-        stepClass={filter[0].StepClass}
-        prepareToLearn={filter[0].prepareToLearn}
-      />
-    </div>
-  );
-};
+// const Dynamic = () => {
+//   const { query } = useRouter();
+//   const { data, error, isLoading } = useSWR("/api/class", fetcher);
+//   console.log(error, isLoading);
+//   const filter = data.data.classes.filter((items: any) => items.title == query.classes?.at(0));
+//   return (
+//     <div>
+//       <DetailClasses
+//         title={query.classes?.at(0)}
+//         mentorixPoin={filter[0].mentorixPoin}
+//         level={filter[0].difficultyLevel}
+//         time={filter[0].time}
+//         lesson={filter[0].lesson}
+//         desc={filter[0].aboutClass}
+//         stepClass={filter[0].StepClass}
+//         prepareToLearn={filter[0].prepareToLearn}
+//       />
+//     </div>
+//   );
+// };
 
-export default Dynamic;
+// export default Dynamic;
