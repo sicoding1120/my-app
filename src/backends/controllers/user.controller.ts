ad60876e60c1e53879e3998ece7a8fc57915153b
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-09-15 14:19:35 +0700

    mantep

--- src/backends/controllers/user.controller.ts
+++ src/backends/controllers/user.controller.ts
@@ -1,16 +1,3 @@
-import { userService } from "@/backends/providers/user.service";
-import { NextApiRequest, NextApiResponse } from "next";
-
-export class User extends userService {
-  constructor() {
-    super();
-  }
-
-  async _Login(req: NextApiRequest, res: NextApiResponse) {
-    return this.LoginUser(req, res);
-  }
-
-  async _register(req: NextApiRequest, res: NextApiResponse) {
-    return this.createUser(req, res);
-  }
+export class user {
+  
 }
