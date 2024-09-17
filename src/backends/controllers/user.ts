1456010a31274ec200b085c0e253904a2f7976ee
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-09-17 08:23:32 +0700

    handle error

--- src/backends/controllers/user.ts
+++ src/backends/controllers/user.ts
@@ -1,16 +1,16 @@
-import { userService } from "@/backends/service/user";
-import { NextApiRequest, NextApiResponse } from "next";
+// import { userService } from "@/backends/service/user";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-export class User extends userService {
-  constructor() {
-    super()
-  }
+// export class User extends userService {
+//   constructor() {
+//     super()
+//   }
 
-  async _Login(req: NextApiRequest, res: NextApiResponse) {
-    return this.LoginUser(req,res)
-  }
+//   async _Login(req: NextApiRequest, res: NextApiResponse) {
+//     return this.LoginUser(req,res)
+//   }
 
-  async _register(req: NextApiRequest, res: NextApiResponse) {
-    return this.createUser(req,res)
-  }
-}
+//   async _register(req: NextApiRequest, res: NextApiResponse) {
+//     return this.createUser(req,res)
+//   }
+// }
